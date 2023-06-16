472336d874c2def3f9f96f6a6abbbfd853d984b0
SohanMahadev <sohanm@entrix.in>
2023-06-16 19:38:00 +0530

    add

--- Battery-API/scriptAPI.js
+++ Battery-API/scriptAPI.js
@@ -1,4 +1,4 @@
-const mqtt = require('mqtt');
+/*const mqtt = require('mqtt');
 const { MongoClient } = require('mongodb');
 const express = require('express');
 const moment = require('moment-timezone');
@@ -67,6 +67,90 @@ async function storeBatteryPercentage(device, batteryPercentage) {
     console.error('Error:', err);
   } finally {
     client.close();
+  }
+}
+
+function sendGetBatteryStatusMessage(device) {
+  const message = JSON.stringify({ devID: device, data: 'GB' });
+  client.publish(publishTopic, message);
+  console.log(`GET_BAT_STS message sent for ${device}`);
+}*/
+//kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+
+const mqtt = require('mqtt');
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const moment = require('moment-timezone');
+const mongoose = require('mongoose');
+
+const login = {
+  username: 'mqtt',
+  password: 'mqtt2022',
+  clean: true,
+  connectTimeout: 4000
+};
+
+const brokerUrl = 'mqtt://103.162.246.109:1883';
+
+const publishTopic = 'GET_BAT_STS';
+const subscribeTopic = 'BAT_STS';
+
+const uri = 'mongodb+srv://Sohan:Entrix123@cluster0.ztczrpi.mongodb.net/status?retryWrites=true&w=majority';
+
+const dbName = 'battery';
+const collectionName = 'percentage';
+
+const devices = ['8BEBBFF643C6', 'C87BAAF79BEC', 'C50FC97C91E3', '2450378D18E9', '4D66CDFE31E7'];
+const client = mqtt.connect(brokerUrl, login);
+
+// Define the battery status schema
+const batteryStatusSchema = new mongoose.Schema({
+  device: { type: String, required: true },
+  batteryPercentage: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+// Create the battery status model
+const BatteryStatus = mongoose.model('BatteryStatus', batteryStatusSchema);
+
+client.on('connect', () => {
+  console.log('Connected to MQTT broker');
+  client.subscribe(subscribeTopic);
+  devices.forEach(device => {
+    sendGetBatteryStatusMessage(device);
+  });
+
+  // Sending battery status request every 2 minutes
+  setInterval(() => {
+    devices.forEach(device => {
+      sendGetBatteryStatusMessage(device);
+    });
+  }, 2 * 60 * 1000);
+});
+
+client.on('message', (topic, message) => {
+  if (topic === subscribeTopic) {
+    const payload = JSON.parse(message.toString());
+    const device = payload.devID;
+    const batteryPercentage = payload.data;
+    storeBatteryPercentage(device, batteryPercentage);
+  }
+});
+
+async function storeBatteryPercentage(device, batteryPercentage) {
+  try {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const document = new BatteryStatus({
+      device: device,
+      batteryPercentage: batteryPercentage
+    });
+
+    await document.save();
+    console.log(`Battery percentage for ${device} stored in the database.`);
+  } catch (err) {
+    console.error('Error:', err);
+  } finally {
+    mongoose.disconnect();
   }
 }
 
